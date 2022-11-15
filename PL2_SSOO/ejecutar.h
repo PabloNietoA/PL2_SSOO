58d19761fa0c0c9fa9c123057d72bdecb3d83e53
Pablo Nieto Arias <104018391+PabloNietoA@users.noreply.github.com>
2022-11-15 10:39:48 +0100

    Delete PL2_SSOO directory

--- PL2_SSOO/ejecutar.h
+++
@@ -1,7 +0,0 @@
-#ifndef EJECUTAR_H
-#define EJECUTAR_H
-
-pid_t ejecutar_orden(const char *orden, int *pbackgr);
-void ejecutar_linea_ordenes(const char *orden);
-
-#endif
--- PL2_SSOO/entrada_minishell.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENTRADA_MINISHELL_H
-#define ENTRADA_MINISHELL_H
-
-void leer_linea_ordenes(char *cad);
-void eliminar_salto_linea(char *cad);
-void imprimir_prompt();
-
-#endif
