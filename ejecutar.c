64a6d898516f3054a8ee720ca0a1bd0096ce0e5c
Pablo <pablo.nietoa@edu.uah.es>
2022-11-21 12:18:02 +0100

    Ahora ejecuta ordenes internas de ser necesario

--- ejecutar.c
+++ ejecutar.c
@@ -8,36 +8,44 @@
 #include "parser.h"
 #include "ejecutar.h"
 #include "libmemoria.h"
+#include "internas.h"
 
 
 pid_t ejecutar_orden(const char *orden, int *pbackgr)
 {
-   char **args;
-   pid_t pid;
-   int entrada = 0, salida = 1; //Se guardan los descriptores de archivo (por defecto 0 y 1)
-   int indice_ent = -1, indice_sal = -1; /* por defecto, no hay < ni > */
+   	char **args;
+   	pid_t pid;
+   	int entrada = 0, salida = 1; //Se guardan los descriptores de archivo (por defecto 0 y 1)
+   	int indice_ent = -1, indice_sal = -1; /* por defecto, no hay < ni > */
   
-   if ((args = parser_orden(orden, &indice_ent, &indice_sal, pbackgr)) == NULL)
-   {
-      return(-1);
-   } else {
-   	if(indice_ent != -1) redirec_entrada(args, indice_ent, &entrada);
-   	if(indice_sal != -1) redirec_salida(args, indice_sal, &salida);
-   	pid = fork();
-   	if(pid == -1) {
-		perror("Error en el fork");
-		exit(-1);
-   	} else if (pid == 0) { // si es la minishell hija ejecuta la orden
-   		execvp(args[0], args);
-   		exit(-1);
-   	} else {
-   		free_argumentos(args); //si es la minishell padre devuelve el pid de la hija
-   		return(pid);
+  
+   	if (es_ord_interna(orden)) {
+   			ejecutar_ord_interna(orden);
    	}
-   }
+   	else if ((args = parser_orden(orden, &indice_ent, &indice_sal, pbackgr)) == NULL)
+   	{
+   		return(-1);
+   	} 
+   	else {
+   		if(indice_ent != -1) redirec_entrada(args, indice_ent, &entrada);
+   		if(indice_sal != -1) redirec_salida(args, indice_sal, &salida);
+   		pid = fork();
+   		if(pid == -1) {
+			perror("Error en el fork");
+			exit(-1);
+   		}
+   		else if (pid == 0) { // si es la minishell hija ejecuta la orden
+   			execvp(args[0], args);
+   			exit(-1);
+   		}
+   		else {
+   			free_argumentos(args); //si es la minishell padre devuelve el pid de la hija
+   			return(pid);
+   		}
+   	}
 
-   /* Si la linea de ordenes posee tuberias o redirecciones, podra incluir */
-   /* aqui, en otras fases de la practica, el codigo para su tratamiento.  */
+   	/* Si la linea de ordenes posee tuberias o redirecciones, podra incluir */
+   	/* aqui, en otras fases de la practica, el codigo para su tratamiento.  */
    
 	
 	
