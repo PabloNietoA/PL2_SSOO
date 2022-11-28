490ba1bd6c98c441cfaaaf2b7f6eb6de3c4a88a9
Héctor Valverde de la Plaza <104841580+hectorvalverde096@users.noreply.github.com>
2022-11-28 13:03:47 +0100

    Redirecciones
    hola :)

--- redirecciones.c
+++ redirecciones.c
@@ -5,13 +5,19 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+#include "redirecciones.h"
+
 
 /* funcion que abre el archivo situado en la posicion indice_entrada+1 */
 /* de la orden args y elimina de ella la redireccion completa          */
                                   
 void redirec_entrada(char **args, int indice_entrada, int *entrada)
 {
-	*entrada = open(args[indice_entrada+1],NULL);
+	*entrada = open(args[indice_entrada+1],O_RDONLY); //usamos la flag RDONLY
+	if (*entrada == -1) {
+		printf("Error al abrir el archivo"); //open devuelve -1 si hay error
+		exit(-1);
+	}
 }
 
 /* funcion que abre el archivo situado en la posicion indice_salida+1 */
@@ -19,6 +25,7 @@ void redirec_entrada(char **args, int indice_entrada, int *entrada)
 
 void redirec_salida(char **args, int indice_salida, int *salida)
 {
-	*salida = open(args[indice_salida+1],NULL);
+	*salida = open(args[indice_salida+1],O_RDWR | O_CREAT | O_TRUNC, 0666); //le damos permisos de write y read y 
+										//si no existe lo crea, si existe lo trunca 
 }
 
