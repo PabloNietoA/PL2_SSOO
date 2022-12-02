b8aad843e48e63700bb65f0972862889aa191e38
Héctor Valverde de la Plaza <104841580+hectorvalverde096@users.noreply.github.com>
2022-12-02 12:53:57 +0100

    Entrada y salida como argumentos en ejecutar_orden

--- ejecutar.h
+++ ejecutar.h
@@ -1,7 +1,8 @@
 #ifndef EJECUTAR_H
 #define EJECUTAR_H
 
-pid_t ejecutar_orden(const char *orden, int *pbackgr);
+pid_t ejecutar_orden(const char *orden,int entrada, int salida, int *pbackgr);
 void ejecutar_linea_ordenes(const char *orden);
+int ** crear_pipes(int ordenes);
 
 #endif
